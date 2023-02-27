70149a84511cb00d06992fd0498789b59a314c98
jfvargaproyecto <126501568+jfvargaproyecto@users.noreply.github.com>
2023-02-27 13:43:35 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById('boton_info').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'black';
+});
+document.getElementById('boton_default').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
